4f863f0cc3e1561dff1dd42e01fb2bb06ee09dab
Amanbek <amanbekazizuulu@gmail.com>
2024-11-01 01:21:03 +0600

    example of assertion method usage

--- src/test/java/com/dandaev/edu/MathUtilsTest.java
+++ src/test/java/com/dandaev/edu/MathUtilsTest.java
@@ -1,10 +1,19 @@
 package com.dandaev.edu;
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 public class MathUtilsTest {
 
 	@Test
 	void test(){
-		System.out.println("The test has passed successfully.");
+		MathUtils mathUtils = new MathUtils();
+
+		int expected_value = -1;
+		int actual_value = mathUtils.adder(1, 1);
+
+		assertEquals(expected_value, actual_value);
+
+
 	}
 }
