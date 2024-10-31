2ac544b09f88db3bbb71bd0faa414353fb4ec5d1
Amanbek <amanbekazizuulu@gmail.com>
2024-11-01 01:09:19 +0600

    initial commit

---
+++ src/main/java/com/dandaev/edu/MathUtils.java
@@ -0,0 +1,9 @@
+package com.dandaev.edu;
+public class MathUtils {
+
+	public int adder(int a, int b){
+		return a + b;
+	}
+
+
+}
---
+++ src/test/java/com/dandaev/edu/MathUtilsTest.java
@@ -0,0 +1,10 @@
+package com.dandaev.edu;
+import org.junit.jupiter.api.Test;
+
+public class MathUtilsTest {
+
+	@Test
+	void test(){
+		System.out.println("The test has passed successfully.");
+	}
+}
