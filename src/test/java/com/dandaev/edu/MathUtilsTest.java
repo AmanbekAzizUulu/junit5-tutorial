3b809d970d2dbc43a526a8e4780e090714857386
Amanbek <amanbekazizuulu@gmail.com>
2024-11-02 02:04:15 +0600

    example of using @BeforeAll and @AfterAll annotations was added

--- src/test/java/com/dandaev/edu/MathUtilsTest.java
+++ src/test/java/com/dandaev/edu/MathUtilsTest.java
@@ -4,17 +4,40 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MathUtilsTest {
 	private MathUtils instance_to_test;
+	private static int counter = 0;
+
+	@BeforeAll
+	static void setup(){
+		System.out.println("Setting up resources ....");
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@AfterAll
+	static void release(){
+		System.out.println("Releasing resources ...");
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@BeforeEach
 	void init() {
 		instance_to_test = new MathUtils(); // инициализация объекта тестирования
-		System.out.println("Initializing of test instance ....");
+		System.out.println(++ counter + " — Initializing of test instance ....");
 		try {
 			Thread.sleep(500);
 		} catch (InterruptedException e) {
@@ -24,7 +47,7 @@ public class MathUtilsTest {
 
 	@AfterEach
 	void cleanup() {
-		System.out.println("Cleaning up ....");
+		System.out.println( counter + " — Cleaning up ....");
 		try {
 			Thread.sleep(500);
 		} catch (InterruptedException e) {
@@ -37,8 +60,7 @@ public class MathUtilsTest {
 		// int[] array_1 = new int[] { 0, 1, 2, 3, 4 };
 		// int[] array_2 = new int[] { 4, 3, 2, 1, 0 };
 
-		// assertArrayEquals(array_1, array_2, "given two integer arrays should be
-		// equal");
+		// assertArrayEquals(array_1, array_2, "given two integer arrays should be equal");
 
 		int[] array_3 = new int[] { 5, 6, 7, 8, 9 };
 		int[] array_4 = new int[] { 5, 6, 7, 8, 9 };
@@ -51,14 +73,12 @@ public class MathUtilsTest {
 		// var expected_list_1 = Arrays.asList("apple", "banana", "cherry");
 		// var actual_list_1 = Arrays.asList("coconut", "banana", "cherry");
 
-		// assertIterableEquals(expected_list_1, actual_list_1, "given two lists should
-		// be equal");
+		// assertIterableEquals(expected_list_1, actual_list_1, "given two lists should be equal");
 
 		var expected_list_2 = Arrays.asList("apple", "banana", "cherry");
 		var actual_list_2 = Arrays.asList("apple", "banana", "cherry");
 
 		assertIterableEquals(expected_list_2, actual_list_2, "given two lists should be equal");
-
 	}
 
 	@Test
@@ -82,9 +102,7 @@ public class MathUtilsTest {
 		assertThrows(ArithmeticException.class, () -> instance_to_test.divider(10, 0),
 				"divide by zero should throw ArithmeticException");
 
-		// если же не будет выброшен ArithmeticException, или будет выброшен Exception
-		// отличный от ArithmeticException, тест будет пройден нуспешно
-		// assertThrows(ArithmeticException.class, () -> instance_to_test.divider(10,
-		// 2), "divide by zero should throw ArithmeticException");
+		// если же не будет выброшен ArithmeticException, или будет выброшен Exception отличный от ArithmeticException, тест будет пройден нуспешно
+		// assertThrows(ArithmeticException.class, () -> instance_to_test.divider(10, 2), "divide by zero should throw ArithmeticException");
 	}
 }
